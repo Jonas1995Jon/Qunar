d954b011510792d97af4f594cef31553244a3d79
Jonas <1819745574@qq.com>
2018-11-03 17:44:59 +0800

    add recommend and weekend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,107 @@
+<template>
+	<div>
+		<div class="title">热销推荐</div>
+		<ul>
+			<li 
+				class="item border-bottom" 
+				v-for="item of recommendList"
+				:key="item.id"
+			>
+				<img class="item-img" :src="item.imgUrl">
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-comment">{{item.comment}}条评论</p>
+					<p class="item-desc">{{item.desc}}</p>
+					<button class="item-button">查看详情</button>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HomeRecommend',
+		data () {
+			return {
+				recommendList: [{
+					id: '0001',
+					imgUrl: '//img1.qunarzz.com/sight/p0/1810/36/36ed80c6e147d019a3.img.png_280x200_431528fe.png',
+					title: '重庆两江夜游',
+					comment: '12720',
+					desc: '数十里的山城灯海一览无余，“不览夜景，枉到重庆”'
+				},{
+					id: '0002',
+					imgUrl: '//img1.qunarzz.com/sight/p0/1512/44/44e6f0f838b9adc190.water.jpg_280x200_2b203fbf.jpg',
+					title: '解放碑步行街',
+					comment: '1230',
+					desc: '这里是重庆最繁华的商业圈，也是抗战胜利和重庆解放的历史见证'
+				},{
+					id: '0003',
+					imgUrl: '//img1.qunarzz.com/sight/p0/1409/12/ed83b9bb54ccc7ff1f0989a7ebc2e312.jpg_280x200_18f56996.jpg',
+					title: '洪崖洞',
+					comment: '2290',
+					desc: '逛山城老街、观赏两江风光、品尝当地美食的好去处'
+				},{
+					id: '0004',
+					imgUrl: '//img1.qunarzz.com/sight/p0/1803/63/630540ceb7fb0950a3.water.jpg_280x200_682c250a.jpg',
+					title: '重庆环球金融中心观景台(会仙楼)',
+					comment: '1145',
+					desc: '环玻璃幕墙、全露天设计，感受不一样的梦幻重庆'
+				},{
+					id: '0005',
+					imgUrl: '//img1.qunarzz.com/sight/p0/1705/5c/5c00cf8542ccd71ca3.water.jpg_280x200_94fa7fee.jpg',
+					title: '朝天门',
+					comment: '19',
+					desc: '朝天门是一个码头广场，可以看到两种不同颜色的江水交汇'
+				},]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+	.title
+		font-size: .8rem
+		margin-top: .2rem
+		line-height: 2rem
+		background: #eee
+		text-indent: .3
+	.item
+		font-size: 14px
+		display: flex
+		overflow: hidden
+		align-items: center
+		.item-img
+			width: 140px
+			height: 100px
+			padding: .3rem
+		.item-info
+			flex: 1
+			padding: .3rem
+			min-width: 0
+			.item-title
+				color: #212121
+				font-weight: bold
+				line-height: 1.2rem
+				font-size: 1.2rem
+				ellipsis()
+			.item-comment
+				margin-top: .7rem
+			   color: #9e9e9e
+			   font-size: .75rem
+			   line-height: .75rem
+			.item-desc
+				padding: .5rem 0
+				color: #9e9e9e
+				font-size: .8rem
+				line-height: .8rem
+				ellipsis()
+			.item-button
+				background: #ff9300
+				padding: .1rem .5rem
+				border: none
+				border-radius: .35rem
+				color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,85 @@
+<template>
+	<div>
+		<div class="title">周末去哪儿</div>
+		<ul>
+			<li 
+				class="item border-bottom" 
+				v-for="item of recommendList"
+				:key="item.id"
+			>
+				<div class="item-img-wrapper">
+					<img class="item-img" :src="item.imgUrl">				
+				</div>
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HomeWeekend',
+		data () {
+			return {
+				recommendList: [{
+					id: '0001',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1509/d5/82498669f3a48d.jpg_r_640x214_21f7a4ff.jpg',
+					title: '重庆两江夜游',
+					desc: '数十里的山城灯海一览无余，“不览夜景，枉到重庆”'
+				},{
+					id: '0002',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1505/53/4bcd11cc3a5374.jpg_r_640x214_988a4cee.jpg',
+					title: '重庆必游TOP10',
+					desc: '重庆，非去不可'
+				},{
+					id: '0003',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1505/5f/276ed6784d25ee.jpg_r_640x214_2193082e.jpg',
+					title: '重庆优质汤泉',
+					desc: '细数重庆那些最赞的温泉'
+				},{
+					id: '0004',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1505/48/f117e9b3bf582a.jpg_r_640x214_b8145711.jpg',
+					title: '重庆大美古镇',
+					desc: '行走重庆最美古镇，让你瞬间穿越千年'
+				},{
+					id: '0005',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1506/1e/f97881dbd07520.jpg_r_640x214_f8c25916.jpg',
+					title: '本地人带你耍',
+					desc: '玩转小面之都，High翻天'
+				},]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+	.title
+		font-size: .8rem
+		margin-top: .2rem
+		line-height: 2rem
+		background: #eee
+		text-indent: .3
+	.item-img-wrapper
+		width: auto
+	.item-img
+		width: 100%
+	.item-info
+		padding: .4357rem .625rem .625rem .625rem
+		min-width: 0
+		.item-title
+			padding-right: 4.375rem
+			color: #212121
+			font-size: .875rem
+			line-height: 1.5rem
+			ellipsis()
+		.item-desc
+			padding-right: 4.375rem;
+			color: #616161;
+			font-size: .75rem;
+			line-height: 1.3125rem;
+			ellipsis()
+</style>
