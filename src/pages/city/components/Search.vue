c74258ba14dcbe2095e34665e2a9076a01a73a51
Jonas <1819745574@qq.com>
2018-11-04 18:47:10 +0800

    city search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,76 @@
 <template>
-	<div class="search">
-		<input class="search-input" type="text" name="search" placeholder="输入城市名或拼音">
+	<div>
+		<div class="search">
+			<input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音">
+		</div>
+		<div 
+			class="search-content" 
+			ref="search"
+			v-show="keyword"
+		>
+			<ul>
+				<li 
+					class="search-item border-bottom" 
+					v-for="item of list"
+					:key="item.id"
+				>
+					{{item.name}}
+				</li>
+				<li 
+					class="search-item border-bottom"
+					v-show="hasNoData"
+				>
+					没有找到匹配数据
+				</li>
+			</ul>
+		</div>		
 	</div>
 </template>
 
 <script>
+	import BScroll from 'better-scroll'
 	export default {
 		name: 'CitySearch',
+		props: {
+			cities: Object
+		},
+		data () {
+			return {
+				keyword: '',
+				list: [],
+				timer: null
+			}
+		},
+		computed: {
+			hasNoData () {
+				return !this.list.length
+			}
+		},
+		watch: {
+			keyword () {
+				if(!this.keyword){
+					this.list = []
+					return
+				}
+				if(this.timer){
+					clearTimeout(this.timer)
+				}
+				this.timer = setTimeout(() => {
+					const result = []
+					for (let i in this.cities) {
+						this.cities[i].forEach((value) => {
+							if(value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1){
+								result.push(value)
+							}
+						});
+					}
+					this.list = result
+				}, 100)
+			}
+		},
+		mounted () {
+			this.scroll = new BScroll(this.$refs.search)
+		}
 	}
 </script>
 
@@ -24,5 +88,19 @@
 			line-height: 1rem
 			text-align: center
 			border-radius: .3rem
+			color: #666
+	.search-content
+		z-index: 1
+		overflow: hidden
+		position: absolute
+		top: 4.3rem
+		left: 0
+		right: 0
+		bottom: 0
+		background: #eee
+		.search-item
+			line-height: 2rem
+			padding-left: .625rem
+			background: #fff
 			color: #666
 </style>
