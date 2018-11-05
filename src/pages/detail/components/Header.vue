2f71b9a84661506d4fa86efbe7d5bb93c1a2afa5
Jonas <jiangjian@bangkaoedu.com>
2018-11-05 12:59:37 +0800

    global unbind

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -48,8 +48,11 @@
 				}
 			}
 		},
-		mounted () {
+		activated () {
 			window.addEventListener('scroll', this.handleScroll)
+		},
+		deactivated () {
+			window.removeEventListener('scroll', this.handleScroll)
 		}
 	}
 </script>
